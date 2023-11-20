76fe928b246a4b5f11f34c1cb35008da8e410120
Giriraju B <125765239+Giriraju7@users.noreply.github.com>
2023-11-20 19:06:00 +0530

    Add files via upload

---
+++ TASK 5 - BATCH 3 APPLICANT DETAILS/Query.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Batch3_Applicant (
+   Full_Name varchar(200), 
+   Gender varchar(10),
+   Qualification varchar(200), 
+   Degree varchar(200), 
+   Pass_Out_Year varchar(10), 
+   College_Name varchar(300) 
+);
